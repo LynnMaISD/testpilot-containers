a9e2d598fb5f32ea3fffb4d20abf9c34d4ba7d13
groovecoder <luke.crouch@gmail.com>
2017-05-02 12:07:20 -0500

    fix npm test/lint failures

--- study.js
+++ study.js
@@ -25,18 +25,13 @@ class ContainersStudy extends shield.Study {
   }
 
   whenEligible () {
-    console.log("ContainersStudy.whenEligible()");
   }
 
   whenInstalled () {
-    console.log("ContainersStudy.whenInstalled()");
-    console.log("shield variation: ", this.variation);
     tabs.open(`data:text/html, Thank you for helping us study Containers in Firefox. You are in the ${this.variation} variation.`);
   }
 
-  cleanup(reason) {
-    console.log("ContainersStudy.cleanup()");
-    console.log(reason);
+  cleanup() {
   }
 }
 
